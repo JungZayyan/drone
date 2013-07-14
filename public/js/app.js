9993fe50123c46df4206c088a5594d6816f7bdd7
Thomas Martineau <mart0657@gmail.com>
2013-07-14 12:16:08 -0400

    Added inital app.js file

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+/*
+ * app will be the namesapce for the application
+ */
+
+(function (app, $) {
+	app.init = function () {
+
+		//Setup listeners for ui
+
+		//Setup socket events
+
+		//Setup the canvas
+	}
+
+	$(document).ready(function () {
+
+		//When the dom is loaded then initialize the app
+		app.init();
+	});
+})(var app = app || {}, jQuery)
