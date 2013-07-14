5fc250af1c9d168b6da133549b97546882818c64
Thomas Martineau <mart0657@gmail.com>
2013-07-14 12:48:39 -0400

    Added setup stuff and initial canvas functionality

--- public/js/app.js
+++ public/js/app.js
@@ -3,18 +3,55 @@
  */
 
 (function (app, $) {
-	app.init = function () {
+    /***********************
+    ** Setup functions
+    ***********************/
+    function setupSocketEvents () {
+        app.socket.on('png update', function (data) {
 
-		//Setup listeners for ui
+            //Assuming that we are getting base64 render that to the canvas
+            app.streamCanvas.render(data.png);
+        });
+    }
+    function setupUiListeners () {
 
-		//Setup socket events
+    }
 
-		//Setup the canvas
-	}
+    /***********************
+    ** Canvas class
+    ***********************/
+    function Canvas (canvasEl) {
+        this.el = canvasEl;
+        this.context = this.el.getContext('2d');
+    }
+    Canvas.prototype.render = function (imgData) {
+        var img = new Image();
+        
+        console.log(imgData);
+        img.onload = function () {
+            this.context.drawImage(this, 0, 0, this.el.width, this.el.height);
+        }
+        img.src = imgData;
+    }
 
-	$(document).ready(function () {
+    app.init = function () {
+        var streamCanvas = document.getElementById('png-stream'),
+        
+        app.socket = io.connect('http://localhost');
 
-		//When the dom is loaded then initialize the app
-		app.init();
-	});
+        //Setup listeners for ui
+        setupUiListeners();
+
+        //Setup socket events
+        setupSocketEvents();
+
+        //Setup the canvas
+        app.streamCanvas = new Canvas(streamCanvas);
+    }
+
+    $(document).ready(function () {
+
+        //When the dom is loaded then initialize the app
+        app.init();
+    });
 })(var app = app || {}, jQuery)
